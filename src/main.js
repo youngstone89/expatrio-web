cd0220722737f5ee35df1cb599eb639c2ed4f3b9
youngstone <youngstone89@gmail.com>
2020-01-22 22:58:20 +0900

    signup and login tested, but role hardcoded

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { router } from './router';
+import store from './store';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import { ValidationProvider } from 'vee-validate';
+import VeeValidate from 'vee-validate';
+
+import Vuex from 'vuex';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+    faHome,
+    faUser,
+    faUserPlus,
+    faSignInAlt,
+    faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
+
+Vue.use(VeeValidate);
 
 Vue.config.productionTip = false
+// Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.use(Vuex);
+
+
 
 new Vue({
+    router,
+    store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Login from './views/Login.vue';
+import Register from './views/Register.vue';
+
+Vue.use(Router);
+
+export const router = new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/register',
+            component: Register
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            // lazy-loaded
+            component: () => import('./views/Profile.vue')
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            // lazy-loaded
+            component: () => import('./views/BoardAdmin.vue')
+        },
+        {
+            path: '/customer',
+            name: 'customer',
+            // lazy-loaded
+            component: () => import('./views/BoardCustomer.vue')
+        }
+    ]
+});
